7672338e56a82d930da40f77d9793d6c42d71310
Pilipenko Oleksandr <sanikpilipenko@gmail.com>
2026-05-07 13:29:14 +0300

    Update Predator.java

--- src/domain/Predator.java
+++ src/domain/Predator.java
@@ -1,8 +1,7 @@
 package domain;
 
 public class Predator extends Animal {
-
     public void hunt() {
-        System.out.println("Predator is hunting...");
+        System.out.println("Predator is searching for prey...");
     }
 }
